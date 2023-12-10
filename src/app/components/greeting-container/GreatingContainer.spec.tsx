e29ace09ff0013112d8ec4c3dff5a14d9f63609e
Sammy Ferreira <sammyfe2021@gmail.com>
2023-12-10 12:18:53 -0300

    :heavy_check_mark: test: files for button and greeting container components

--- src/app/components/greeting-container/GreatingContainer.spec.tsx
+++ src/app/components/greeting-container/GreatingContainer.spec.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import GreatingContainer from './GreatingContainer'
 
 describe('Greating Container', () => {
@@ -10,11 +10,18 @@ describe('Greating Container', () => {
   })
   it('should render after click', () => {
     const { getByText } = render(<GreatingContainer />)
+    screen.getByText('Hello World')
 
     const buttonElement = getByText('Change Text')
+
     fireEvent.click(buttonElement)
 
     const UpdateTextComponent = getByText('Bye World')
+    screen.queryByText('Hello World')
+
+    const oldMessage = screen.queryByText('Hello World')
+    expect(oldMessage).toBeNull()
+    expect(oldMessage).not.toBeInTheDocument()
 
     expect(UpdateTextComponent).toBeInTheDocument()
   })
